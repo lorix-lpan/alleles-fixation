abbbb1079377982a65f9a42a6db4041f41dcbefb
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2015-11-06 21:28:09 -0500

    layout is a bit messed up

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from allele import Allele
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QApplication,
-        QLabel)
+        QLabel, QGridLayout)
 
 class Window(QWidget):
 
@@ -10,18 +10,31 @@ class Window(QWidget):
         self.initUI()
 
     def initUI(self):
+        # Create grid layout
+        self.grid = QGridLayout()
+        self.grid.setSpacing(10)
+
         # Create an intance of the Allele class
         self.alle = Allele()
 
         lbl1 = QLabel(str(self.alle._freq), self)
-        lbl1.move(15, 10)
+        btnCross = QPushButton('Cross', self)
 
-        self.btn = QPushButton('Cross', self)
-        self.btn.move(200,10)
+        btnCross.clicked.connect(self.startCrossing)
 
+        self.grid.addWidget(btnCross, 0, 0)
+        self.grid.addWidget(lbl1, 2, 0)
+
+        self.setLayout(self.grid)
         self.setGeometry(300, 200, 500, 400)
         self.setWindowTitle('Alleles Fixation Simulation')
         self.show()
+
+    def startCrossing(self):
+        if not self.alle._isFixated:
+            self.alle.cross()
+        self.grid.addWidget(QLabel(str(self.alle._freq),self), 3, 0)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
